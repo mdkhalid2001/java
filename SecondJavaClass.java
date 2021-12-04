9d99a2f3350de8a2f7e0554b1db23330882b8342
Md Hasibul Khalid Shuvo <91559654+mdkhalid2001@users.noreply.github.com>
2021-12-04 10:42:59 -0800

    This is the first branch commit

---
+++ SecondJavaClass.java
@@ -0,0 +1,9 @@
+
+public class SecondJavaClass {
+    public static void main(String[] args) {
+        System.out.println("1 \t  2 \n3 \t  8");
+        // System.out.println("3 \t  4");
+         //System.out.println("3 \t  6");
+    }
+  
+}
