1bdd1d947a3ece4dc1730a3a014f163402a28fd2
Md Hasibul Khalid Shuvo <91559654+mdkhalid2001@users.noreply.github.com>
2021-12-04 10:45:26 -0800

    ALL in first branch

--- SecondJavaClass.java
+++ SecondJavaClass.java
@@ -3,7 +3,32 @@ public class SecondJavaClass {
     public static void main(String[] args) {
         System.out.println("1 \t  2 \n3 \t  8");
         // System.out.println("3 \t  4");
-         //System.out.println("3 \t  6");
+         System.out.println("3 \t  6");
     }
   
-}
+}git checkout -b hotfic
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+x
